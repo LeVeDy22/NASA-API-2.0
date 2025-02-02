ba99eabe5cf6357ef4257b11afdce4233ce1debf
unknown <kostyapysmenny2@gmail.com>
2025-02-02 19:14:20 +0200

    update 1

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
+from flask import Flask, render_template, request
 from get_result import get_data
-from flask import Flask
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    result_json = get_data("2025-02-02")
-    return result_json
+    return render_template("get_data.html")
+
+
+@app.route("/result_json", methods=["POST"])
+def result_json():
+    date = request.form.get("date")
+    try:
+        return get_data(date=date)
+    except ValueError:
+        return "Error"
 
 if __name__ == "__main__":
     app.run(debug=True)
--- get_result.py
+++ get_result.py
@@ -10,8 +10,10 @@ def get_data(date):
     hazardous_asteroids = dict()
 
     url = f"https://api.nasa.gov/neo/rest/v1/feed?start_date={date}&end_date={date}&api_key={API}"
-    result = requests.get(url=url).json()["near_earth_objects"][date]
-
+    result = requests.get(url=url).json()["near_earth_objects"]
+    if result == {}:
+        return "Немає даних."
+    result = result[date]
     for i in range(len(result)):
         if result[i]["is_potentially_hazardous_asteroid"] is True:
             hazardous_asteroids[i] = result[i]
